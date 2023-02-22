fd040978e5b289dd2f96027c7cbe6aea2502ef22
Mike Randazzo <slashrandazzo@gmail.com>
2023-02-22 08:51:49 -0800

    Update App.svelte
    I also think an explanation of what 'import {marked} from 'mark' does for this portion would be helpful.

--- site/content/tutorial/06-bindings/05-textarea-inputs/app-b/App.svelte
+++ site/content/tutorial/06-bindings/05-textarea-inputs/app-b/App.svelte
@@ -10,3 +10,5 @@
 <style>
 	textarea { width: 100%; height: 200px; }
 </style>
+<!-- There is no difference in the output betweeen the 'show me' and the 'reset' outputs. I think if you want to show that there is a two-way binding between
+ the value and textarea, there should be an option to change the value in both places. I'm new to this and hope this is a valuable input. -->
